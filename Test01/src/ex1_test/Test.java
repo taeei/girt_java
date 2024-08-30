0a29d377829da4e984a86b54b3ba31ddc9b3f1b1
user1 <user1@DESKTOP-SMNIJCG>
2024-08-30 17:22:43 +0900

    ㅁㅁㅁ

---
+++ Test01/src/ex1_test/Test.java
@@ -0,0 +1,8 @@
+package ex1_test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("240830 테스트111");
+	}
+
+}
---
+++ Test01/src/ex2_test/Test1.java
@@ -0,0 +1,8 @@
+package ex2_test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println(" 240830 테스트");
+	}
+
+}
